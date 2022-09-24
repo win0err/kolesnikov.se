787f23fd27f7a7fec074d89636cca3b76be9cd18
Sergei Kolesnikov <sergei@kolesnikov.se>
2022-09-24 13:26:54 +0300

    Add news about the life in my blog

--- src/index.php
+++ src/index.php
@@ -22,6 +22,7 @@
                 <h2>News</h2>
 
                 <ul>
+                    <li>There's <a href="/blog/there-is-life-here.html">life on my blog</a> now</li>
                     <li>
                         Hello, world! I finished with a new version of the site. It took about 3 months for me. <br />
                         If you find typos or grammatical inaccuracies on my website, please <a href="/contacts.html">contact me</a>.
