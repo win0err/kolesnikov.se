b3aac05283d28d663f7ac9aeb69eae473e0c0922
Sergei Kolesnikov <sergei@kolesnikov.se>
2024-04-19 00:43:05 +0300

    Update Now page

--- src/now.php
+++ src/now.php
@@ -21,52 +21,39 @@
 
 				<ul>
 					<li>
-						Since the end of January, I was looking for a new job.
-						Starting in March, I’ll be working at VK.com. Wish me luck!
-					</li>
-
-					<li>
-						I finally got the motivation to finish and defend my PhD dissertation. <br />
-						I plan to start by publishing several scientific articles.
-					</li>
-
-					<li>I’m pleased with myself because I’ve started shooting more portraits.</li>
-
-					<li>
 						I am reading too many books in parallel:
 						<ul>
-							<li>
-								<details>
-									<summary>
-										A collection of short stories by Ryūnosuke Akutagawa — finished.
-									</summary>
-
-									<p>My recommendations are:</p>
-
-									<ol>
-										<li>Hell Screen</li>
-										<li>The Story of a Head That Fell Off</li>
-										<li>Mori Sensei</li>
-										<li>In a Grove</li>
-										<li>Otomi’s Virginity</li>
-										<li>A Clod of Earth</li>
-										<li>Spinning Gears</li>
-									</ol>
-								</details>
-							</li>
 							<li>“Theater of the World: The Maps that Made History” by Thomas Reinertsen Berg</li>
 							<li>“Bullshit Jobs. A Theory” by David Graeber</li>
-							<li>“For Whom the Bell Tolls” by Ernest Hemingway</li>
+							<li>“For Whom the Bell Tolls” by Ernest Hemingway — <img src="/assets/attention/finished.gif" alt="finished" />, it was incredible</li>
 							<li>Re-reading <abbr title="Introduction to Algorithms by Cormen, Leiserson, Rivest, Stein">CLRS</abbr></li>
 						</ul>
 					</li>
 
-					<li>I’ve learned how to play <i>Cage The Elephant — Trouble</i> on guitar.</li>
+					<li>I received a wonderful postcard and painting from Bodø, Norway. D.S., if you are reading this, thanks a lot!</li>
 
+					<li>I just finished playing <i>GTA: San Andreas</i>, and now I’m playing <i>GTA: Vice City</i> on my <a href="/about.html#uses" title="Uses">19-inch Lacie Electron Blue IV</a> CRT monitor.</li>
 
-					<li>I (still) play Skyrim on my PlayStation 5 and Xonotic on my laptop (you can find me on Jeff’s Deadmatch server). I recently bought a 19-inch Lacie Electron Blue IV CRT monitor to play classic games. I really enjoyed playing Fahrenheit: Indigo Prophecy Remastered on this monitor. Started playing Heavy Rain.</li>
+					<li>
+						I’m finishing the fourth season of <i>The Wire</i> and occasionally the fourth season of <i>Sex and the City</i>.
+						I’m planning to catch up on the second season of <i>Twin Peaks</i> and the fourth season of <i>The Sopranos</i> later on.
+					</li>
 
-					<li>I just finished watching the fourth season of True Detective, and I think the final episodes are just terrible. I’m currently watching season four of The Sopranos, season three of We Bare Bears, and occasionally the third season of Sex and the City. I’m going to continue watching the second season of Twin Peaks and the third season of The Wire later on.</li>
+					<li>
+						I’ve bought an AE prism finder for my Mamiya RZ67, waiting for sunny days to test it out!
+					</li>
+
+					<li>
+						This month I have plans for the release of a new version of <a href="https://github.com/win0err/gnome-runcat/" target="_blank" rel="noopener noreferrer nofollow">GNOME RunCat</a> with a number of new features..
+					</li>
+
+					<li>
+						I’m still <del>procrastinating</del> writing a research paper on my PhD research.
+					</li>
+
+					<li>
+						Finally, I’m currently working as a software engineer at VK.com.
+					</li>
 				</ul>
 
 			</main>
