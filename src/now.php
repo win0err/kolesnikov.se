d259a177006403ee2dfbec00c1b686e67e3a48e3
Sergei Kolesnikov <sergei@kolesnikov.se>
2024-10-02 10:31:12 +0300

    Update Now page with summer updates

--- src/now.php
+++ src/now.php
@@ -23,9 +23,21 @@
 
 				<p>
 					I am reading too many books in parallel:
-					“Theater of the World: The Maps that Made History” by Thomas Reinertsen Berg,
-					“Bullshit Jobs. A Theory” by David Graeber,
-					and re-reading <abbr title="Introduction to Algorithms by Cormen, Leiserson, Rivest, Stein">CLRS</abbr>.
+				</p>
+
+				<ul>
+					<li>“One Hundred Years of Solitude” by Gabriel García Márquez,</li>
+					<li>“Bullshit Jobs. A Theory” by David Graeber,</li>
+					<li>“Discipline and Punish: The Birth of the Prison” by Michel Foucault.</li>
+				</ul>
+
+				<p>
+					I’ve finally finished reading “Theater of the World: The Maps that Made History” by Thomas Reinertsen Berg.
+					This book tells the history of mapmaking from ancient times to the present. It looks fantastic and is well written, but in my opinion, it lacks many of the maps referenced in the text. Whenever I read about a map, I had to go on the internet to see what it looks like.
+				</p>
+
+				<p>
+					I’ve also finished “The Old Man and the Sea” by Ernest Hemingway. I admire the simplicity and style of Hemingway’s writing.
 				</p>
 
 
@@ -34,40 +46,37 @@
 				<h3>PC</h3>
 
 				<p>
-					From time to time, I play <i>NFS: Most Wanted</i> on my <a href="/about.html#uses" title="Uses">19-inch Lacie Electron Blue IV</a> CRT monitor,
-					but I still have plans to play <i>GTA III</i>, as I want to complete the original trilogy of the GTA series.
+					From time to time, I play <i>GTA III</i> and <i>NFS: Most Wanted</i> on my <a href="/about.html#uses" title="Uses">19-inch LaCie Electron Blue IV</a> CRT monitor.
 				</p>
 
 
 				<h3>Consoles</h3>
 
 				<p>
-					I finished playing <i>Heavy Rain</i> on PlayStation 3. It’s okayish, but in my opinion, <i>Fahrenheit: Indigo Prophecy</i> is a lot better than the more famous <i>Heavy Rain</i>.
-				</p>
-
-				<p>
-					My PlayStation collection has grown: I bought a <i>PlayStation Vita</i>. So now I have a PlayStation 3, 4, 5, and Vita.
-					I must admit, I got hooked on <i>Persona 4 Golden</i> on Vita.
-					I also started <i>Final Fantasy HD</i>, but decided not to play it for now because I don’t even have enough time for Persona 4.
-					Since my Vita has a <i>PlayStation Portable</i> emulator, I would also like to play <i>GTA: Liberty City Stories</i> and <i>GTA: Vice City Stories</i>.
+					I’ve spent 74 hours playing <i>Persona 4 Golden</i> on my <i>PlayStation Vita</i>.
+					Since my Vita has a <i>PlayStation Portable</i> emulator, I also play <i>GTA: Vice City Stories</i> from time to time.
 				</p>
 
 
 				<h2>Watching Movies & TV Series</h2>
 
+				<ul>
+					<li><i>The Wire</i> <img src="/assets/attention/finished.gif" alt="finished" /> — one of the best TV series I have ever seen;</li>
+					<li><i>The Sopranos</i> — I’m currently in the middle of the sixth season;</li>
+					<li><i>Tokyo Vice</i> — I started watching the second season; I really like the visuals of that series;</li>
+					<li><i>Gravity Falls</i> — just started;</li>
+					<li><i>Sex and the City</i> — the sixth season has finally become cringe comedy. Absolutely awful.</li>
+				</ul>
+
 				<p>
-					I was recommended to watch “Jojo Rabbit”, and I was expecting it to be an average movie, but it turned out to be surprisingly good.
-					I’m finishing the fifth season of <i>The Wire</i> and also the fifth season of <i>Sex and the City</i>, but I’ve also started watching <i>Tokyo Vice</i>.
-					I plan to catch up on the second season of <i>Twin Peaks</i> and the fourth season of <i>The Sopranos</i> later on.
+					I plan to catch up on the second season of <i>Twin Peaks</i> later on.
 				</p>
 
 
 				<h2>Taking Photos</h2>
 
 				<p>
-					I had a great photoshoot in June, and the model turned out to be an incredibly interesting person. I’m eagerly waiting for the next photoshoot.
-					I’m considering posting the photos from these photoshoots on the website.
-					After a lot of searching, I finally found a great deal on a Nikon 24/1.4G lens and bought it.
+					I bought a Nikon 28Ti film camera!
 				</p>
 
 
@@ -75,15 +84,18 @@
 
 				<p>This month I <del>still</del> have plans for the release of a new version of <a href="https://github.com/win0err/gnome-runcat/" target="_blank" rel="noopener noreferrer nofollow">GNOME RunCat</a> with a number of new features.</p>
 
+				<p>I started developing Quite Simple Image Editor as a demonstration of how linear algebra can be used for image processing.</p>
+
+				<p>
+					There are some plans for this blog.
+					First of all, I’m going to publish a photo post with summer photos.
+					Secondly, I’m thinking about posting photos of the cool gadgets I use on the <a href="/about.html#uses">Uses</a> page.
+				</p>
+
 
 				<h2>Doing Science</h2>
 
 				<p>I’m still <del>writing a research paper on my PhD research</del> procrastinating.</p>
-
-
-				<h2>Traveling</h2>
-
-				<p>I’ve visited Saint Petersburg this month.</p>
 
 
 				<h2>Working</h2>
